7aeb4c2e2a4af06986979456efb8847b099f2ccb
c0de4un <code4un@yandex.ru>
2022-04-02 09:08:52 +0400

    Add linter and git-hook for it

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,21 +1,7 @@
-import { Engine } from "./engine/Engine";
-
 window.onload = () => {
     console.log('Starting game . . .');
 
-    let engine: Engine = new Engine();
 
-    window.onresize = () => {
-        engine.onResize();
-    };
-
-    engine.Start();
 
     console.log('Game is closed');
-}
-
-/**
- * Start App
- */
-export function StartApp() {
 }
